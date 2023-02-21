11ef73e23b31e9be40be1daacad648b228a788f5
Christopher Jones <jonesch7@oregonstate.edu>
2023-02-20 20:25:02 -0800

    updated weather.py

--- Weather.py
+++ Weather.py
@@ -16,21 +16,21 @@ import webbrowser
 import math
 import io
 import datetime
+import math
+import config
 
 
 class WeatherMapping:
-    def __init__(self, start_address, end_address, weather_type, travel_date=datetime.date.today().strftime("%Y-%m-%d"), num_of_checks=5):
+    def __init__(self, start_address, end_address, weather_type, travel_date=datetime.date.today().strftime("%Y-%m-%d")):
         # initialize endpoints and api keys
-        self.endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-        self.gmaps_key = ''
-        self.googlemaps = googlemaps.Client(self.gmaps_key)
-        self.owm_key = ""
+        self.endpoint = "https://maps.googleapis.com/maps/api/directions/json?"
+        self.googlemaps = googlemaps.Client(config.gmaps_key)
+        self.owm_key = config.owm_key
 
         self.start_address = start_address.replace(' ', '+')
         self.end_address = end_address.replace(' ', '+')
         self.weather_type = weather_type
         self.date_of_travel = travel_date
-        self.num_of_checks = num_of_checks
 
         self.nav_request = None
         self.request = None
@@ -45,19 +45,19 @@ class WeatherMapping:
         self.gps_coords = None
         self.Row_list = None
 
-    def get_route_coords(self):
+    def _get_route_coords(self):
         """
         makes request to google directions api and creates a pandas dataframe to hold lat/lon pairs of route
         """
 
         # make request and get response
-        self.nav_request = f"""origin={self.start_address}&destination={self.end_address}&key={self.gmaps_key}"""
+        self.nav_request = f"""origin={self.start_address}&destination={self.end_address}&key={config.gmaps_key}"""
         self.request = self.endpoint + self.nav_request
         self.response = urllib.request.urlopen(self.request).read()
         self.directions = json.loads(self.response)
 
         # create empty dataframe. lat = 0, lng = 1
-        self.route_gps_coords = pd.DataFrame(columns=[0, 1])
+        route_gps_coords = pd.DataFrame(columns=[0, 1])
 
         # determine number of steps in route
         self.steps_in_route = len(
@@ -65,126 +65,70 @@ class WeatherMapping:
 
         # put gps coords and add to dataframe
         for i in range(self.steps_in_route):
-            self.route_gps_coords = pd.concat([self.route_gps_coords,
-                                               pd.DataFrame(polyline.decode((self.directions['routes'][0]['legs'][0]['steps'][i]['polyline']['points'])))])
+            route_gps_coords = pd.concat([route_gps_coords,
+                                          pd.DataFrame(polyline.decode((self.directions['routes'][0]['legs'][0]['steps'][i]['polyline']['points'])))])
 
         # reset index
-        self.route_gps_coords = self.route_gps_coords.reset_index(drop=True)
+        route_gps_coords = route_gps_coords.reset_index(drop=True)
 
-        return (self.route_gps_coords)
-
-    def extract_weather(self, weather_response):
-        """
-        input weather response from openweathermap's api
-        date_of_travel is a string of the travel date yyyy-mm-dd
-        output is dataframe temp, weather description, city id and date as index
-        """
-
-        # create empty dataframe. lat = 0, lng = 1
-        self.weather_at_checkpoint = pd.DataFrame(
-            columns=['date', 'temp', 'description', 'city_id'])
-
-        len_of_forecast = len(weather_response['list'])
-
-        city_id = int(weather_response['city']['id'])
-
-        # extract gps coords and add to dataframe
-        for i in range(len_of_forecast):
-            # get description
-            description = weather_response['list'][i]['weather'][0]['main']
-
-            # get datetime
-            date = weather_response['list'][i]['dt_txt']
-
-            # get temp
-            temp = weather_response['list'][i]['main']['temp']
-            temp = (temp - 273.15) * 9/5 + 32
-
-            # make list
-            row_i = pd.DataFrame([[date, temp, description, city_id]], columns=[
-                                 'date', 'temp', 'description', 'city_id'])
-
-            # append to dataframe
-            self.weather_at_checkpoint = pd.concat(
-                [self.weather_at_checkpoint, row_i])
-
-            # reset index
-            self.weatheweather_at_checkpointr_df = self.weather_at_checkpoint.reset_index(
-                drop=True)
-            self.weather_at_checkpoint['city_id'] = self.weather_at_checkpoint['city_id']
-
-        self.weather_at_checkpoint.set_index(pd.to_datetime(
-            self.weather_at_checkpoint['date'].values), inplace=True)
-        self.weather_at_checkpoint.drop('date', axis=1, inplace=True)
-        self.weather_at_checkpoint = self.weather_at_checkpoint.loc[self.date_of_travel]
-
-        return (self.weather_at_checkpoint)
-
-    def get_all_weather(self):
-        """
-        Docstring
-        """
-        self.gps_coords = self.get_route_coords()
-
+        # get route coordinates as a "row list"
         self.Row_list = []
 
-        # Iterate over each row
-        for rows in self.gps_coords.itertuples():
+        for rows in route_gps_coords.itertuples():
             # Create list for the current row
             my_list = [rows[1], rows[2]]
 
             # append the list to the final list
             self.Row_list.append(my_list)
 
-        self.possible_marker_locations = []
+        return(route_gps_coords)
 
-        for i in range(1, len(self.gps_coords),  math.floor(len(self.gps_coords)/self.num_of_checks)):
-            self.possible_marker_locations.append({'name': '{}'.format(i),
-                                                   'location': (self.gps_coords.iloc[i].values[0], self.gps_coords.iloc[i].values[1])})
+    def _check_weather(self, lat, lon):
+        """
+        Docstring
+        """
 
-        self.all_checkpoints_weather = []
+        owm_request = f'''https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={ self.owm_key}'''
 
-        for i in range(self.num_of_checks):
-            owm_request = 'https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}'.format(
-                self.possible_marker_locations[i]['location'][0], self.possible_marker_locations[i]['location'][1], self.owm_key)
+        owm_response = urllib.request.urlopen(owm_request).read()
+        weather_response = json.loads(owm_response)
 
-            owm_response = urllib.request.urlopen(owm_request).read()
-            weather_response = json.loads(owm_response)
+        for i in weather_response['list']:
+            try:
+                if i[self.weather_type]:
+                    return (True)
+            except KeyError:
+                pass
 
-            self.all_checkpoints_weather.append(
-                self.extract_weather(weather_response))
+        return (False)
 
-        return (self.all_checkpoints_weather)
+    def _get_checkpoint_locations(self):
+        """
+        this function finds the locations (lat/lon) to check for weather along a route. every 10 miles.
+        return: list 
+        """
+        checkpoint_locations = []
+        route_length = int(
+            self.directions['routes'][0]['legs'][0]['distance']['text'].split(' ')[0])
+        self.number_of_checkpoints = math.ceil(route_length/10)
 
-    def popup_locations(self):
-        # format for inserting into folium popup: '<a href=" [URL LINK HERE] "target="_blank"> [TEXT GOES HERE]' </a>'
+        for i in range(1, len(self.route_gps_coords),  math.floor(len(self.route_gps_coords)/self.number_of_checkpoints)):
+            checkpoint_locations.append({'name': f"""{i}""",
+                                         'location': (self.route_gps_coords.iloc[i].values[0], self.route_gps_coords.iloc[i].values[1])})
 
-        self.get_all_weather()
-        self.popup_info = []
-
-        for i in range(len(self.all_checkpoints_weather)):
-            # if self.all_checkpoints_weather[i]['description'].str.contains(self.weather_type).any():
-            if (self.all_checkpoints_weather[i]['description'] == self.weather_type).any():
-                # The above code is the original code. i tried removing the .str
-                popup_location = self.possible_marker_locations[i]['location']
-                popup_city_id = self.all_checkpoints_weather[i]['city_id'][0]
-                url = 'https://openweathermap.org/city/{}'.format(
-                    self.all_checkpoints_weather[i]['city_id'][0])
-                weblink = '<a href=" {} "target="_blank"> {} {}'.format(
-                    url, 'Link to weather 5 day forecast', '</a>')
-                self.popup_info.append(
-                    [popup_location, popup_city_id, weblink])
-
-        return (self.popup_info)
+        return (checkpoint_locations)
 
     def create_map(self):
+        # get the gps coordinates for the route.
+        self.route_gps_coords = self._get_route_coords()
 
-        # popup_locations function collects weather from all checkpoints and returns information for producing popup on map
-        self.popup_locations()
-
-        # center map at center of route
+        # create and center map at center of route
         self.map = folium.Map(location=self.Row_list[round(
             len(self.route_gps_coords)/2)], zoom_start=8)
+
+        # add route polylines to map
+        folium.PolyLine(locations=self.Row_list,
+                        line_opacity=0.5).add_to(self.map)
 
         # starting marker
         folium.Marker(self.Row_list[0], popup='Origin').add_to(self.map)
@@ -193,25 +137,30 @@ class WeatherMapping:
         folium.Marker(self.Row_list[-1],
                       popup='Destination').add_to(self.map)
 
-        # add marker for weather location
-        for i in range(len(self.popup_info)):
-            iframe = folium.IFrame(
-                html=self.popup_info[i][2], width=200, height=75)
-            popup = folium.Popup(iframe, max_width=2650)
+        # get checkpoint locations
+        self.checkpoint_locations = self._get_checkpoint_locations()
 
-            folium.Marker(self.popup_info[i][0],
-                          popup=popup).add_to(self.map)
+        # add markers to map
+        for i in self.checkpoint_locations:
 
-        folium.PolyLine(locations=self.Row_list,
-                        line_opacity=0.5).add_to(self.map)
+            if self._check_weather(i['location'][0], i['location'][1]) == True:
+
+                url = f'''https://openweathermap.org/city/{self.checkpoint_locations['name']}'''
+                weblink = f'''<a href=" {url} "target="_blank"> {'Link to weather 5 day forecast'} {'</a>'}'''
+
+                iframe = folium.IFrame(
+                    html=url, width=200, height=75)
+                popup = folium.Popup(iframe, max_width=2650)
+
+                folium.Marker(weblink, popup=popup).add_to(self.map)
 
         return self.map
 
 
-def weather_map(origin, destination, weather_type, travel_date, num_of_checks):
+def weather_map(origin, destination, weather_type, travel_date):
     # create WeatherMapping object
     w = WeatherMapping(origin, destination, weather_type,
-                       travel_date, num_of_checks)
+                       travel_date)
 
     map = w.create_map()
 
@@ -222,5 +171,5 @@ def weather_map(origin, destination, weather_type, travel_date, num_of_checks):
     webbrowser.open('map.html')
 
 
-WeatherMapping.produce_map(
-    "Seattle, WA", "Moses Lake, WA", "Snow", "2/6/2023", 10)
+weather_map("Seattle, WA", "Moses Lake, WA",
+            "Clouds", datetime.datetime.today().strftime('%Y-%m-%d'))
