9ba261fbb51a1efa47131953b26c3f63046a334d
Christopher Jones <jonesch7@oregonstate.edu>
2023-03-13 19:40:29 -0700

    updated

--- WeatherRouteApp.py
+++ WeatherRouteApp.py
@@ -9,6 +9,14 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 
 class ApplicationInformation(QDialog):
+    """
+    Applications first screen. Contains a description of the application and its limitations.
+    Parameters:
+        None
+    Returns:
+        None
+    """
+
     def __init__(self):
         super(ApplicationInformation, self).__init__()
 
@@ -18,7 +26,7 @@ class ApplicationInformation(QDialog):
         self.setMaximumWidth(800)
         self.layout = QVBoxLayout()
 
-        description_label = QLabel("Welcome to my CS361 portfolio project! This applications name is Weather Routing and its purpose is to find weather along a driving route. You will be asked to provide a starting and ending city/address/location, choose the type of weather to search for (three options provided) and your travel date (1-4 days from current day). \n The driving route is provided using Google Maps Directions API. The forecast data is provided using a microservice created by my partner, Jared Chang, which calls OpenWeatherMap's 5 Day / 3 Hour Forecast.\nLimitations: 1) Addresses must be able to be ")
+        description_label = QLabel("Welcome to my CS361 portfolio project! This applications name is Weather Routing and its purpose is to find weather along a driving route. You will be asked to provide a starting and ending city/address/location, choose the type of weather to search for (three options provided) and your travel date (1-4 days from current day).\nThe driving route is provided using Google Maps Directions API. The forecast data is provided using a microservice created by my partner, Jared Chang, which calls OpenWeatherMap's 5 Day / 3 Hour Forecast.\nLimitations: 1) Addresses must be connected via roadway system 2) Weather is checked every 20 miles along route 3) Only 'snow', 'clouds' and 'rain' may be searched for along route.")
         description_label.setStyleSheet("font-size: 13pt;")
         description_label.setWordWrap(True)
         self.layout.addWidget(description_label, Qt.AlignCenter)
@@ -39,13 +47,16 @@ class ApplicationInformation(QDialog):
             "font-size: 14pt; border-radius: 10px; background-color: rgba(0, 217, 199, 190)"
             "}"
         )
-        self.open_mainwindow_button.clicked.connect(self.handle_login)
+
+        self.open_mainwindow_button.clicked.connect(self.open_mainwindow)
         self.layout.addWidget(self.open_mainwindow_button, Qt.AlignCenter)
 
-        #
         self.setLayout(self.layout)
 
-    def handle_login(self):
+    def open_mainwindow(self):
+        """
+        accept signal for QDialog 
+        """
         self.accept()
 
     def exit_app(self):
@@ -78,15 +89,12 @@ class MainWindow(QMainWindow):
 
         self.setStyleSheet(
             "border-radius: 20px; \
-        background-color: qlineargradient(spread:pad, x1:0.091, y1:0.101636, x2:0.991379,y2:0.977, stop:0 rgba(209, 107, 165, 255), stop:1 rgba(255,255,255,255));")
+        background-color: qlineargradient(spread:pad, x1:0.091, y1:0.101636, x2:0.991379,y2:0.977, stop:0 rgba(240, 163, 231, 255), stop:1 rgba(255,255,255,255));")
 
-        # Widget
         self.centralwidget = QWidget(self)
         self.setWindowTitle('Weather On Route')
         self.resize(800, 600)
         self.layout = QFormLayout()
-
-        self._create_menu_bar()
 
         self.start_address_widget = QLineEdit()
         self.start_address_widget.setPlaceholderText("Example: Seattle, WA")
@@ -114,8 +122,7 @@ class MainWindow(QMainWindow):
         self.number_of_checks.setStyleSheet(
             "font-size: 14pt; border-color: none none white none; border: 1.5px; background-color: rgba(0,0,0,0);")
 
-        # Add description of what to enter by user
-        directions_text = f"""To find your route directions and the weather along it, please enter your starting address and ending address\nNext choose the type of weather you would like search for along your route using the available options in the drop down menu.\nFinally select your date of travel (within 10 days of present day), then click Produce Map\nYou can make any changes to the form and clicking Produce Map will produce the adjusted map."""
+        directions_text = f"""To find your route directions and the weather along it, please enter your starting address and ending address\nNext choose the type of weather you would like search for along your route using the available options in the drop down menu.\nFinally select your date of travel (1-4 days of present day), then click Produce Map\nYou may make changes to the form and clicking Find Weather Along Route will produce the adjusted map."""
         directions = QLabel(directions_text)
         directions.setStyleSheet(
             "font-size: 10pt; background-color: rgba(0,0,0,0%);")
@@ -141,7 +148,6 @@ class MainWindow(QMainWindow):
             "font-size: 14pt; background-color: rgba(0,0,0,0%);")
         self.layout.addRow(self.travel_date_label, self.travel_date_widget)
 
-        # adding pushbutton
         self.pushButton = QPushButton()
         self.pushButton.setText("Find Weather Along Route")
         self.pushButton.setStyleSheet(
@@ -151,87 +157,88 @@ class MainWindow(QMainWindow):
                                 border-style: solid; \
                                 border-radius: 10px; \
                                 border-width: 1.5px; \
-                                border-color: white; \
-                                background-color: rgb(145, 255, 246)"
+                                background-color: rgb(0, 255, 234)"
             "}"
-            "QPushButton::pressed"
+            "QPushButton::hover"
             "{"
             "font-size: 14pt; \
                                 border-style: solid; \
                                 border-radius: 10px; \
                                 border-width: 1.5px; \
-                                border-color: white; \
-                                background-color: rgb(0, 217, 199)"
+                                background-color: rgb(153, 255, 247)"
             "}"
         )
         self.pushButton.setGeometry(QRect(200, 150, 93, 28))
 
-        # adding signal and slot
+        # connect push button to create and display map function
         self.pushButton.clicked.connect(self.create_and_display_map)
         self.layout.addWidget(self.pushButton)
 
-        # add widget for housing html
+        # view/edit html doc
         self.browser = QWebEngineView()
 
-        # add layout to widget and set as central widget
         widget = QWidget()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)
 
-    def _get_user_input(self):
-        """
-        
-        """
-        self.start_address = self.start_address_widget.text()
-        self.end_address = self.end_address_widget.text()
-        self.weather_type = self.weather_type_widget.currentText()
-        self.travel_date = self.travel_date_widget.date().toPyDate().strftime("%Y-%m-%d")
-
     def create_and_display_map(self):
+        """
+        Generates and displays map
+        Parameters:
+            None
+        Returns:
+            None
+        """
 
         self._get_user_input()
 
-        # create map
         try:
             wmap = Weather.WeatherMapping(
                 self.start_address, self.end_address, self.weather_type, self.travel_date)
             self.map = wmap.create_map()
 
-            # for testing
-            self.map.save('testmap.html', close_file=False)
-
             # save map data
             data = io.BytesIO()
             self.map.save(data, close_file=False)
 
-            # get map data
+            # embed map data into widget then display
             self.browser.setHtml(data.getvalue().decode())
             self.layout.addWidget(self.browser)
-            self.resize(1250, 950)
+            self.resize(1200, 850)
 
+        # this is for catching an exception from the weather class.
+        # if the user inputs locations that cannot be connected via roadways, google api will not return data
         except Exception as e:
             if str(e) == 'list index out of range':
-                e = 'Google Directions API could not create driving directions with the addresses provided.\nPlease ensure the addresses are possible to drive between.'
+                e = 'Google Directions API could not create driving directions with the addresses provided.\nPlease ensure the addresses real and connected via roadways.'
             msgbox = QMessageBox()
             msgbox.setWindowTitle("Error Creating Map")
             msgbox.setText(f"Error when creating map \n{e}")
             msgbox.setStandardButtons(QMessageBox.Ok)
             msgbox.exec()
 
-    def _create_menu_bar(self):
-        menuBar = self.menuBar()
-        menuBar.setStyleSheet("background-color: rgb(255,255,255)")
-        menuBar.addMenu(QMenu("&File", self))
-        menuBar.addMenu(QMenu("&Settings", self))
-        menuBar.addMenu(QMenu("&Help", self))
+    def _get_user_input(self):
+        """
+        Assigns user input from form fields to class variables 
+        Parameters:
+            None
+        Returns:
+            None
+        """
+        self.start_address = self.start_address_widget.text()
+        self.end_address = self.end_address_widget.text()
+        self.weather_type = self.weather_type_widget.currentText()
+        self.travel_date = self.travel_date_widget.date().toPyDate().strftime("%Y-%m-%d")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    login = ApplicationInformation()
+    # display info screen
+    app_info_screen = ApplicationInformation()
 
-    if login.exec_() == QDialog.Accepted:
+    # display main window when user clicks button on info screen
+    if app_info_screen.exec_() == QDialog.Accepted:
         main_window = MainWindow()
         main_window.show()
         sys.exit(app.exec_())
